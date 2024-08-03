b34c9efd6d5ce76b1638c2ca7872002d12155ef9
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-03 18:20:40 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,8 +1,40 @@
-let score="33abcd";
+// let score="33abcd";
 
-// let value=parseInt(score);
-let value=Number(score);
+// // let value=parseInt(score);
+// let value=Number(score);
 
-console.log(typeof(value));
+// console.log(typeof(value));
+
+
+// ***********************operations*******************
+
+// let isvalue =3;
+// let  negvalue=-isvalue
+// console.log(negvalue,"negvalue");
+
+// let str1 ="mayank"
+// let str2="singh";
+// let str3=str1+str2;
+// console.log(str3);
+
+// let num1=1
+// let num2="2"
+// let num3=num1+num2;
+// console.log(num3,"number is ");
+
+//console.log(-true);
+let x = 3;
+const y = x++;
+
+//console.log(x);
+console.log(`x:${x}, y:${y}`);
+
+// let a = 3;
+// const b = ++a;
+
+// console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
 
 
