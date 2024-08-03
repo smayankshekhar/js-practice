b3d3dc033c4e526e4ae2df59c86ad72797bd4649
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-03 17:15:10 +0000

    variable and datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,8 @@
+const accoundId=1443;
+let accountEmail="smayanksehkhar@gmail.com";
+var accountPwd="1234";
+city="gurgaon";
+console.log(typeof(accoundId),"<><>");
+console.log(typeof(city),"<><>");
+console.table([accoundId,accountEmail,accountPwd,city]);
+console.table([accoundId,accountEmail,accountPwd,city]);
