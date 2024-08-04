f5990e80a8788f33cca6e94ce6807a937ac823e6
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-04 07:51:16 +0000

    strings

---
+++ 01_basics/string.js
@@ -0,0 +1,13 @@
+// const name="mayank";
+// const surname="singh";
+// console.log(`name is ${name}`);
+
+//const GameName="mayank";
+const GameName=String("/Mayank Shekhar Singh   ")
+
+//console.log(GameName.trim());
+
+console.log(GameName.replace('/','h'));
+
+//console.log(GameName.replaceAll(/\s/g,''));
+
