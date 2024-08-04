7168cfdb49a0d449b064f171eb1cd30f17f2478b
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-04 06:31:49 +0000

    datatypes_summary

---
+++ 01_basics/comparison.js
@@ -0,0 +1,7 @@
+// console.log("2">1);
+// console.log(null>undefined);
+// console.log(undefined>null);
+// console.log(undefined===null);
+
+
+
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,20 @@
+ // primitive 
+//  String,Number,Boolean,null,undefined,Symbol,BigInt
+
+//reference (NOn-primitive)
+
+//array,objects,functions
+
+const outsideTemp="null";
+let Useremail;
+const id=Symbol('123');
+const newId =Symbol('123');
+console.log(id==newId);
+const bigtype=1234n;
+const val='1234';
+console.log(bigtype==val);
+console.log(typeof(id));
+
+
+//console.log(typeof(bigtype));
+
