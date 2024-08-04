7ce40d4d2f624ec4eb03fe261366599fcfe94960
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-04 07:06:25 +0000

    stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -21,16 +21,34 @@
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-//stack memory is primitive datatype 
+// stack memory is primitive datatype 
 // heap memory is in non-premitive datatype
-//let name=mayank;
+// let name=mayank;
 // let another_name= name;
 // name="singh";
 
 // let test1='mayank'
 // console.log(test1);
 
-console.log('hello');
+// console.log('hello');
+let test1='mayank';
+let test2=test1;
+test1='shekhar'
+console.log(test1);
+console.log(test2);
+
+
+// for heap 
+let a={
+    userName:"mayank",
+    userMail:"smayankshekhar@gmail.com",
+    userphone:"23"
+}
+
+let b=a;
+b.userName='mayank shekhar saingh';
+console.log(b.userName);
+console.log(a.userName);
 
 
 
