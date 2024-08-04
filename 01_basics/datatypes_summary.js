b8338ead14d4b2dd07b7b2a492314d0c4144ccd3
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-04 06:50:51 +0000

    error

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -5,16 +5,35 @@
 
 //array,objects,functions
 
-const outsideTemp="null";
-let Useremail;
-const id=Symbol('123');
-const newId =Symbol('123');
-console.log(id==newId);
-const bigtype=1234n;
-const val='1234';
-console.log(bigtype==val);
-console.log(typeof(id));
+// const outsideTemp="null";
+// let Useremail;
+// const id=Symbol('123');
+// const newId =Symbol('123');
+// console.log(id==newId);
+// const bigtype=1234n;
+// const val='1234';
+// console.log(bigtype==val);
+// console.log(typeof(id));
 
 
 //console.log(typeof(bigtype));
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+//stack memory is primitive datatype 
+// heap memory is in non-premitive datatype
+//let name=mayank;
+// let another_name= name;
+// name="singh";
+
+// let test1='mayank'
+// console.log(test1);
+
+console.log('hello');
+
+
+
+
+
 
