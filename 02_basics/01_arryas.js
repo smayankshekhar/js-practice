426565d641e3db6767440696c62e4374103f6063
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-06 16:18:42 +0000

    arrays_learn.js

---
+++ 02_basics/01_arryas.js
@@ -0,0 +1,21 @@
+const myarr=[0,1,2,3,4,5,6]
+// console.log(myarr.length);
+// console.log(myarr[0]);
+// myarr.push(8);
+// myarr.pop()
+// console.log(myarr);
+// myarr.unshift();
+// myarr.shift()
+
+//slice ,splice 
+//splice manupulates orginal array 
+
+const myarr1=myarr.slice(1,3);
+console.log("A",myarr1);
+const myarr2=myarr.splice(1,3);
+console.log("B",myarr1);
+console.log(myarr2);
+
+
+
+
