140b557a15b66e830ec6d5b988a52cb033143839
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-07 18:28:34 +0000

    IIFE_ FOR DB CONNECTION

---
+++ 02_basics/iife.js
@@ -0,0 +1,12 @@
+//immediatley invoked function expression (IIFE)
+(function db (){
+    console.log("db connected ");
+    
+})();
+
+((DB2)=>{
+console.log("DB 2 CONNECTED ");
+
+})();
+
+//BE PRECISE WITH SEMI COLON AS IF IT IS NOT USED IT WILL THROW ERROR;
