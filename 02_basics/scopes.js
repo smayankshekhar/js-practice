d137edf583202ef98ce302924eb7127a0c4b1a65
smayankshekhar <46066908+smayankshekhar@users.noreply.github.com>
2024-08-07 17:50:11 +0000

    functions and scopes

--- 02_basics/scopes.js
+++ 02_basics/scopes.js
@@ -8,3 +8,33 @@ if (true){
 // console.log(a);
 console.log(c);
 
+
+function one() {
+    const username="mayank";
+
+    function two() {
+        const website="youtube";
+        console.log(username);
+        
+        
+    }
+   // console.log(website);
+    two()
+}
+one()
+
+//hoisting and scope concept
+
+function one(value) {
+    return value+1
+    
+}
+console.log(one(1))
+
+console.log(two(2));
+const two =function secondfunc(value) {
+
+    return value+3;
+    
+}
+//console.log(two(2));
